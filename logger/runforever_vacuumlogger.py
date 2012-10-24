ae5068a70ef79649eee5385fef54e7a80ccfbd78
Gergely Imreh <imrehg@gmail.com>
2012-10-24 18:58:17 +0800

    logger: add new runforever_* scripts for vacuum/iongauge/thermologger

---
+++ logger/runforever_vacuumlogger.py
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+CMD="./vacuumlogger.py vacuumlogger.conf >> vacuum.csv"
+TEXT="Vacuumlogger restarted"
+EMAIL=imrehg@gmail.com
+
+trap ctrl_c INT
+function ctrl_c() {
+    kill ${PID}
+    echo "Finished with " ${PID}
+    exit
+}
+
+function run_cmd() {
+    echo "${TEXT}" | mail -s 'Bakeout' ${EMAIL}
+    echo ${CMD}
+    eval ${CMD}
+    PID=$!
+    echo ${PID}
+}
+
+run_cmd
+while true; do
+    while kill -0 "$PID"; do
+	sleep 0.5
+    done
+    echo "Killed!"
+    run_cmd
+done
