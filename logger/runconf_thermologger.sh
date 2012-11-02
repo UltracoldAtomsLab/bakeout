85b2c589f65d38c8f1270bdbf297af0860d52110
Gergely Imreh <imrehg@gmail.com>
2012-11-02 16:58:13 +0800

    logger: thermologger command had to be modified to get the right PID

--- logger/runconf_thermologger.sh
+++ logger/runconf_thermologger.sh
@@ -1,2 +1,2 @@
 NAME="Thermologger"
-CMD="rmmod -w cdc_acm && modprobe cdc_acm && ./thermologger.py thermologger.conf > thermologger.csv &"
+CMD="rmmod -w cdc_acm | modprobe cdc_acm | ./thermologger.py thermologger.conf >> thermologger.csv &"
