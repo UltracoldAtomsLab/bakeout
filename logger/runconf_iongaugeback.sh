b47ce99f7140542dd574a33e91c70a32032952fc
Gergely Imreh <imrehg@gmail.com>
2012-11-02 11:02:21 +0800

    remove my email from the source files

--- logger/runconf_iongaugeback.sh
+++ logger/runconf_iongaugeback.sh
@@ -1,4 +1,3 @@
 NAME="Ion Gauge Back"
 CMD="./iongauge01.py iongauge1.conf >> iongauge1.csv &"
 TEXT="${Name} restarted"
-EMAIL="imrehg@gmail.com"
--- logger/runconf_ionpump1.sh
+++ logger/runconf_ionpump1.sh
@@ -1,4 +1,3 @@
 NAME="External Ion Pump"
 CMD="./ionpump01.py ionpump1.conf >> ionpump1.csv &"
 TEXT="${NAME} restarted"
-EMAIL="imrehg@gmail.com"
