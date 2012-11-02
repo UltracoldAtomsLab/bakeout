b287463454ed049b4f6ce00cfcdc88f004ad6b5d
Gergely Imreh <imrehg@gmail.com>
2012-11-02 17:07:44 +0800

    logger: remove some unused files

--- logger/relog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TERM="EMI?"
-
-echo ">> Started"
-while true; do
-  journalctl -afb -n0 | grep ${TERM} -m1 && echo "found" && ./restart.sh
-done
--- logger/restart.sh
+++
@@ -1,3 +0,0 @@
-
-# do correct things
-rmmod -w cdc_acm && modprobe cdc_acm && echo "EMI - restarted" | mail -s 'Bakeout' imrehg@gmail.com && ./thermologger.py thermologger.conf > thermolog4000.csv &
