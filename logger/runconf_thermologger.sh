948af3b580cbe683aaac3836ac86c758d18d71e9
Gergely Imreh <imrehg@gmail.com>
2013-03-25 17:01:46 +0800

    thermologger: don't do kernel module unload/load anymore

--- logger/runconf_thermologger.sh
+++ logger/runconf_thermologger.sh
@@ -1,2 +1,6 @@
 NAME="Thermologger"
-CMD="(rmmod -w cdc_acm && modprobe cdc_acm); ./thermologger.py thermologger.conf >> thermologger.csv &"
+## Can't use this if there is more than one ACM device connected
+#CMD="(rmmod -w cdc_acm && modprobe cdc_acm); ./thermologger.py thermologger.conf >> thermologger.csv &"
+
+## Might have to manually run rmmod / modprobe when needed
+CMD="./thermologger.py thermologger.conf >> thermologger.csv &"
