925730162f58d75e26a63cb3271deb501b48831e
Gergely Imreh <imrehg@gmail.com>
2013-03-26 15:11:08 +0800

    logger: update thermologger to play better with others via lockfile

--- logger/thermologger.py
+++ logger/thermologger.py
@@ -6,6 +6,7 @@ import sys
 import pymongo
 import datetime
 import ConfigParser
+import os
 
 config = ConfigParser.SafeConfigParser({'baud': 115200,
                                         'hosts': 'localhost:27017',
@@ -27,20 +28,82 @@ database = config.get('Database', 'database')
 
 # Check which port to log on
 baud = config.getint('Gauge', 'baud')
-serialnum = config.getint('Gauge', 'com')
-dev = serial.Serial('/dev/ttyACM%d' %(serialnum), baud, timeout=1)
+
+combase = 'ttyACM'  # the base name of USB device
+
+class Thermologger:
+    """ Thermologger device """
+
+    teststring= 'T,'
+
+    def __init__(self, com=None, baud=115200):
+        found = False
+        self.dev = None
+        if com:
+            try:
+                self.dev = serial.Serial("/dev/%s" %(com),
+                                         baud,
+                                         timeout=1,
+                                         )
+                found = True
+            except serial.SerialException:
+                errormsg = "Can't connect to %s" %(com)
+        else:
+            for port in range(0, 10):
+                try:
+                    portname = "/dev/%s%d" %(combase, port)
+                    self.lockfile = portname.split('/')[-1] + '.lock'
+                    if os.path.exists(self.lockfile):
+                        continue
+                    else:
+                        open(self.lockfile, 'w').close()
+                    self.dev = serial.Serial(portname,
+                                             baud,
+                                             timeout=1,
+                                             )
+                    # Need to do read-delay for Arduino Mega ADK, otherwise
+                    # we get a lot of previous junk
+                    delay = 0.2
+                    finish = time.time() + delay
+                    while time.time() < finish:
+                        self.dev.readline()
+                    # Have to test substring, since the reply is checksummed
+                    # and change according to the given instrument ID
+                    r = self.dev.readline()
+                    if len(r) > 2 and r[0:len(self.teststring)] == self.teststring:
+                        print "# Thermologger on ttyUSB%d" %(port)
+                        found = True
+                        break
+                    if os.path.exists(self.lockfile):
+                        os.remove(self.lockfile)
+                except serial.SerialException:
+                    if os.path.exists(self.lockfile):
+                        os.remove(self.lockfile)
+                    self.lockfile = None
+                    continue
+            if not found:
+                errormsg = "Can't find correct USB"
+        if not found:
+            raise IOError(errormsg)
+
+    def readline(self):
+        return self.dev.readline()
+
+    def cleanup(self, debug=True):
+        if self.lockfile and os.path.exists(self.lockfile):
+            if debug:
+                print "# Removing lockfile"
+            os.remove(self.lockfile)
+        else:
+            if debug:
+                print "# No lockfile to clean up"
+
+
+dev = Thermologger()
 
 connection = pymongo.Connection(mongos)
 db = connection[database]
 coll = db.readings
-
-# Need to do read-delay for Arduino Mega ADK, otherwise
-# we get a lot of previous junk
-delay = 0.05
-finish = time.time() + delay
-while time.time() < finish:
-    dev.readline()
-
 
 # Keep reading until Ctrl-C
 print "#HotJunction(C),ColdJunction(C)"
@@ -50,7 +113,7 @@ while True:
         if len(reading) > 0:
             date = datetime.datetime.utcnow()
             try:
-                idnum, tc, cc, ctime, err = reading.strip().split(",")
+                id, idnum, tc, cc, ctime, err = reading.strip().split(",")
                 idnum, tc, cc = int(idnum), float(tc), float(cc)
                 idnum = idnum + 100  # for new monitoring
                 document = {"type": "temperature",
@@ -79,4 +142,8 @@ while True:
         print "# Trying AutoReconnect"
         continue
     except KeyboardInterrupt:
+        dev.cleanup()
         break
+    except:
+        dev.cleanup()
+        raise
